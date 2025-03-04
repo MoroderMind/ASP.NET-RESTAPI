d788d33a0293aeeadc414c61bf9a852ccd607f0b
MoroderMind <moroderwow@gmail.com>
2025-03-04 21:48:34 +0100

    bug

--- GameStoreAPI/obj/Debug/net9.0/GameStoreAPI.AssemblyInfo.cs
+++ GameStoreAPI/obj/Debug/net9.0/GameStoreAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3726ea4c4fff2d18b4f7d01159f56bac0829f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
